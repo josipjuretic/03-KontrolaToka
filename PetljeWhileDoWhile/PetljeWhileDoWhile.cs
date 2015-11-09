55ad2b44f7b18153c972b9fefbb48f48a03e7741
josipjuretic <juretic.josip@gmail.com>
2015-11-09 21:37:02 +0100

    Code review V.1.2.2

--- PetljeWhileDoWhile/PetljeWhileDoWhile.cs
+++ PetljeWhileDoWhile/PetljeWhileDoWhile.cs
@@ -9,17 +9,19 @@ namespace Vsite.CSharp
             Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
 
             int brojBacanja = 0;
-            // TODO: Napisati simulaciju početka igre "Čovječe ne ljuti se": kocka se baca sve dok ne padne na 6
-
+            //  DO while TODO: Napisati simulaciju početka igre "Čovječe ne ljuti se": kocka se baca sve dok ne padne na 6
+            do
             {
                 ++brojBacanja;
                 int slučajniBroj = generatorSlučajnih.Next(1, 6); // generira slučajni broj između 1 i 6
                 Console.WriteLine("{0}. pokušaj: {1}", brojBacanja, slučajniBroj);
             }
 
-            int brojPređenihPolja = brojBacanja;
-            // TODO: Napisati petlju u kojoj se kocka baca sve dok brojPređenihPolja ne postane veći od 12
+            while (brojBacanja < 6);
 
+            int brojPređenihPolja = brojBacanja;
+            // while TODO: Napisati petlju u kojoj se kocka baca sve dok brojPređenihPolja ne postane veći od 12
+            while (brojPređenihPolja > 12);
             {
                 int slučajniBroj = generatorSlučajnih.Next(1, 6); // generira slučajni broj između 1 i 6
                 Console.WriteLine("{0} + {1}", brojPređenihPolja + slučajniBroj);
